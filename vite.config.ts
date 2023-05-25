ed70b5d7ab7f3b8422bdadf7603c4ec00c7488f5
DRMus <78694252+DRMus@users.noreply.github.com>
2023-05-25 18:19:44 +0400

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/Dungeon-game/"
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/Dungeon-game/'
+  }
+
+  return config
 })
+
